9c364336031bdfa92c8b913ff7c87c7d0c242096
TanakulW <108814478+TanakulW@users.noreply.github.com>
2024-03-12 15:58:40 +0700

    up

--- my-introduce/src/page/homePageP/component/contentComp.jsx
+++ my-introduce/src/page/homePageP/component/contentComp.jsx
@@ -1,18 +1,16 @@
 import { Box } from "@mui/material";
 import React from "react";
+import CardContent from "../component/cardContent"
+
 
 const contentComp = (props) => {
-    const {darkModeMain}=props
-    
+  const { darkModeMain } = props;
+
   return (
     <Box
       sx={{
-        backgroundColor: darkModeMain
-              ? "#666666"
-              : " #cccccc",
-              backgroundImage: darkModeMain
-              ? "none"
-              : "#666666",
+        backgroundColor: darkModeMain ? "#666666" : " #cccccc",
+        backgroundImage: darkModeMain ? "none" : "#666666",
         height: "100vh",
         display: "flex",
         justifyContent: "center",
@@ -21,16 +19,17 @@ const contentComp = (props) => {
     >
       <Box
         sx={{
-          backgroundColor: "skyblue",
+            backgroundColor: darkModeMain ? "#595959" : "  #d9d9d9",
+            backgroundImage: darkModeMain ? "none" : " #595959",
           width: "60vw",
-          height:"80vh",
+          height: "80vh",
           display: "flex",
           justifyContent: "center",
-          boxShadow: "2px 2px 0px 0px #4A1C04",
-          borderRadius:"20px"
+          boxShadow: "2px 2px 0px 0px black",
+          borderRadius: "20px",
         }}
       >
-        contentComp
+<CardContent/>
       </Box>
     </Box>
   );
